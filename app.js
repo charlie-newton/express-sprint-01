970a4b32f169ad24b07e1f0c72314389e23e0904
charlie-newton <146384118+charlie-newton@users.noreply.github.com>
2023-10-25 20:51:48 +0100

    Added fix for CORS issue

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ import database from "./database.js";
 // Configure express app --------------------------
 const app = new express();
 
-// Configure middleware ---------------------------
+// Configure middleware --------------------------
+
+app.use(function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+    next();
+  
+  });
+  
+   
+  
+  app.use(cors({ origin: '*' }));
 
 // Controllers ------------------------------------
 const projectsController = async (req, res) => {
