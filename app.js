27765c935b1bf8d8573a6117342d6d4d7c3b1286
charlie-newton <146384118+charlie-newton@users.noreply.github.com>
2023-11-29 23:06:44 +0000

    Added update and delete methods

--- app.js
+++ app.js
@@ -47,14 +47,40 @@ const postProjectsController = async (req, res) => {
     res.status(201).json(projectResult);
 }
 
+const putProjectsController = async (req, res) => {
+    const id = req.params.id;
+    const record = req.body;
+
+    const sql = buildProjectsUpdateSql();
+    const { isSuccess, result, message: accessorMessage } = await update(sql, id, record);
+    if(!isSuccess) return res.status(400).json({ message: accessorMessage });
+
+    res.status(200).json(result);
+}
+
+const deleteProjectsController = async (req, res) => {
+    const id = req.params.id;
+    const record = req.body;
+
+    const membersSql = buildMembersProjectsDeleteSql();
+    const { isSuccess: memberSuccess, result: memberResult, message: memberAccessorMessage } = await deleteEntry(membersSql, id);
+    if(!memberSuccess) return res.status(400).json({ message: memberAccessorMessage });
+
+    const sql = buildProjectsDeleteSql();
+    const { isSuccess, result, message: accessorMessage } = await deleteEntry(sql, id);
+    if(!isSuccess) return res.status(400).json({ message: accessorMessage });
+
+    res.status(200).json({ message: accessorMessage });
+}
+
 // Builders ----------------------
 const buildSetFields = (fields) => fields.reduce((setSql, field, index) =>
     setSql + `${field}=:${field}` + ((index === fields.length - 1) ? '' : ', '), `SET ` );
 
 const buildProjectsSelectSql = (id) => {
     let table = `projects`;
-    let fields = ["projects.projectID, projects.projectName, projects.projectDescription, projects.projectImage, projects.projectDeadline"];
-    let sql = `SELECT ${fields} FROM ${table} WHERE projects.projectID = ${id}`;
+    let fields = ["projectID, projectName, projectDescription, projectImage, projectDeadline"];
+    let sql = `SELECT ${fields} FROM ${table} WHERE projectID = ${id}`;
 
     return sql;
 }
@@ -69,8 +95,8 @@ const buildUsersProjectsSelectSql = (id) => {
 
 const buildMembersSelectSql = (id) => {
     let table = `members`;
-    let fields = ["members.memberID, members.userID, members.projectID"];
-    let sql = `SELECT ${fields} FROM ${table} WHERE members.memberID = ${id}`;
+    let fields = ["memberID, userID, projectID"];
+    let sql = `SELECT ${fields} FROM ${table} WHERE memberID = ${id}`;
 
     return sql;
 }
@@ -85,6 +111,22 @@ const buildMembersInsertSql = () => {
     let table = `members`;
     let mutableFields = ['userID', 'projectID'];
     return `INSERT INTO ${table} ` + buildSetFields(mutableFields);
+}
+
+const buildProjectsUpdateSql = () => {
+    let table = `projects`;
+    let mutableFields = ['projectName', 'projectDescription', 'projectImage', 'projectDeadline'];
+    return `UPDATE ${table} ` + buildSetFields(mutableFields) + ` WHERE projectID=:projectID`;
+}
+
+const buildProjectsDeleteSql = () => {
+    let table = `projects`;
+    return `DELETE FROM ${table} WHERE projectID=:projectID`;
+}
+
+const buildMembersProjectsDeleteSql = () => {
+    let table = `members`;
+    return `DELETE FROM ${table} WHERE members.projectID=:projectID`;
 }
 
 // CRUD --------------------------
@@ -117,11 +159,47 @@ const read = async (sql) => {
     }
 }
 
+const update = async (sql, id, record) => {
+    try {
+        const status = await database.query(sql, { ...record, projectID: id } );
+
+        if (status[0].affectedRows === 0)
+            return { isSuccess: false, result: null, message: `Failed to update record: no rows affected` }
+
+        const recoverRecordSql = buildProjectsSelectSql(id);
+
+        const { isSuccess, result, message } = await read(recoverRecordSql);
+
+        return isSuccess
+            ? { isSuccess: true, result: result, message: "Record successfuly recovered" }
+            : { isSuccess: false, result: null, message: `Failed to recover the updated record: ${message}` }
+    } 
+    catch (error) {
+        return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` }
+    }
+}
+
+const deleteEntry = async (sql, id) => {
+    try {
+        const status = await database.query(sql, { projectID: id } );
+
+        return status[0].affectedRows === 0
+            ? { isSuccess: false, result: null, message: `Failed to delete record ${id}` }
+            : { isSuccess: true, result: null, message: "Record sucessfully deleted" }
+    } 
+    catch (error) {
+        return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` }
+    }
+}
+
 // Endpoints --------------------------------------
-app.get("/api/projects", (req, res) => getProjectsController(res, null));
 app.get("/api/projects/users/:id", (req, res) => getProjectsController(req, res));
 
+app.put("/api/projects/:id", (req, res) => putProjectsController(req, res));
+
 app.post("/api/projects/users/:id", postProjectsController);
+
+app.delete("/api/projects/:id", deleteProjectsController);
 
 // Start server -----------------------------------
 const PORT = process.env.PORT || 5000;
